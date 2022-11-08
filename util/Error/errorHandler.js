a210d31d31fbf7da1f6b2ede8e20da3e968118c1
PRAVIN SHARMA <pravinsharma9610@gmail.com>
2022-11-08 21:00:16 +0530

    added success to response

--- util/Error/errorHandler.js
+++ util/Error/errorHandler.js
@@ -1,15 +1,19 @@
-const CustomError = require('./CustomError');
+const CustomError = require("./CustomError");
 
-errorHandler = (err, req, res, next) =>{
-    console.log(err);
+errorHandler = (err, req, res, next) => {
+  console.log(err);
 
-    //handle custmom error
-    if(err instanceof CustomError){
-        return res.status(err.status).json({message: err.message});
-    }
+  //handle custmom error
+  if (err instanceof CustomError) {
+    return res
+      .status(err.status)
+      .json({ message: err.message, success: false });
+  } else {
+    
 
     //handle default error
-    return res.status(500).json({message: err.message});
-}
+    return res.status(500).json({ message: err.message, success: false });
+  }
+};
 
 module.exports = errorHandler;
