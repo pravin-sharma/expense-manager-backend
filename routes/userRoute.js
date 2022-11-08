39e7523dbf0dfe5182892680a2365bb5feceb5aa
PRAVIN SHARMA <pravinsharma9610@gmail.com>
2022-11-08 21:01:42 +0530

    added get user route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,14 @@
-const {register, login} = require('../controller/userController');
+const {register, login, getUser} = require('../controller/userController');
+const {isAuth} = require('../middleware/isAuth')
 const express = require('express');
 const router = express.Router();
 
+
 router.post('/register', register);
 router.post('/login', login);
+router.get('/user', isAuth, getUser);
+
+//update user
+//delete user - admin
 
 module.exports = router;
